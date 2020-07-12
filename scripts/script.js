cf8a71a966464063577e18607e145a38e49e6cae
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-07-12 12:29:36 -0300

    Added newform, close and close

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     var currentId = 1
 
+    
+
     function Note(subject, text, date, checked){
         this.id = currentId;
         currentId++;
@@ -11,10 +13,59 @@ $(document).ready(function(){
     }
 
     function newForm(){
-        
+        $('#add-btn').fadeOut();
+        $('.form').removeClass("hide");
     }
 
+    function closeForm(){
+        $('.form').addClass("hide");
+        $('#add-btn').fadeIn();
+    }
+
+    function save(){
+        let titulo = $('#titulo').val();
+        let texto = $('#textarea').val();
+        let data = $('#datepicker').val();
+        let note = new Note(titulo, texto, data, false)
+        arr.push(note);
+        renderNotes();
+    }
+
+
+    function renderNotes(){
+        $('#main-content').html('');
+        arr.forEach(element => {
+            let el = `<div class="col s12 m6 l4">
+            <div class="card blue-grey darken-1">
+                <div class="card-content white-text">
+                    <span class="card-title">`+ element.subject +`</span>
+                    <p>`+ element.date +`</p>
+                    <p>`+ element.text +`</p>
+                </div>
+                <div class="card-action">
+                    <a href="`+ element.id +`">Edit</a>
+                    <a href="#">Exclude</a>
+                </div>
+            </div>
+        </div>
+        <div class="fixed-action-btn" id="add-btn">
+        <a class="btn-floating btn-large waves-effect waves-light red"><i
+                class="material-icons">add</i></a>
+        </div>`
+            $('#main-content').append(el);
+        });
+    }
+
+    /* Inicialização do contador do textfield*/
+    $('#textarea').characterCounter();
+
+    /*Inicialização do DatePicker*/
+    $('.datepicker').datepicker();
+
+
     d1= new Date();
+
+
 
     /* Test Data */
 
@@ -26,33 +77,42 @@ $(document).ready(function(){
 
     var n4 = new Note('Card Title4', 'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.',d1, true);
 
-    console.log(arr);
+    
 
     var arr = []
-    arr.push(n1);
-    arr.push(n2, n3, n4);
+    arr.push(n1, n2, n3, n4);
 
-    arr.forEach(element => {
-        let el = `<div class="col s12 m6 l4">
-            <div class="card blue-grey darken-1">
-                <div class="card-content white-text">
-                    <span class="card-title">`+ element.subject +`</span>
-                    <p>`+ element.text +`</p>
-                </div>
-                <div class="card-action">
-                    <a href="`+ element.id +`">Edit!</a>
-                    <a href="#">Exclude!</a>
-                </div>
-            </div>
-        </div>`
-        $('#main-content').append(el);
-    });
+    console.log(arr);
 
     
 
+    renderNotes();
+    
 
 
-    $('.fixed-action-btn').click(()=>{
-        newForm()
+    console.log('Tudo bem até aqui')
+    $('#add-btn').on('click',function(){
+        console.log('clicou antes do newform');
+        newForm();
+        console.log('clicou depois do newform');
     })
+
+    
+    /*$('#add-btn').click(()=>{
+        console.log("clicou");
+        newForm();
+
+    })*/
+
+    $('#form-close').click(()=>{
+        closeForm();
+    })
+
+    $('#btn-save').click(()=>{
+        save();
+        closeForm();
+        return false;
+    })
+
+
   });
